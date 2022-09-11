6a8ffc95c10c24df50b2e34ba3b2e952de6356af
cr1m3s <parioleks@gmail.com>
2022-09-11 12:16:42 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from "react";
-import "modern-normalize/modern-normalize.css";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from 'redux-persist/integration/react';
 import store from './redux/store';
-import "./index.css";
-import App from "./App";
-
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,5 +17,5 @@ ReactDOM.render(
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
